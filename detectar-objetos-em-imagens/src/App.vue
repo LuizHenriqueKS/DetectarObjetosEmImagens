eb032eb6c1cf148450563f4ea520eae9f471f6ab
Luiz Henrique Kafer Silva <luizhenrique-rs@windowslive.com>
2020-12-31 17:57:21 -0300

    Upload da imagem

--- detectar-objetos-em-imagens/src/App.vue
+++ detectar-objetos-em-imagens/src/App.vue
@@ -1,29 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <Layout>
+        <template #title>Detectar objetos em imagens</template>
+        <template #view1-title>Imagem crua</template>
+        <template #view2-title>Imagem processada</template>
+        <template #view1>
+          <img ref="rawImg" class="img" />
+        </template>
+        <template #view2>
+          <img ref="processedImg" class="img" />
+        </template>
+        <template #buttons>
+          <v-btn color="primary" @click="carregarImagem">
+            <v-icon small class="mr-2">fas fa-upload</v-icon>
+            Carregar imagem
+          </v-btn>
+        </template>
+      </Layout>
+    </v-main>
+    <input type="file" class="d-none" ref="inputFile" />
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import Layout from "./components/Layout.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    Layout,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  mounted() {
+    const it = this;
+    this.inputFile.addEventListener("change", function () {
+      const it2: any = this;
+      const files = it2.files;
+      if (files && files[0]) {
+        it.rawImg.src = URL.createObjectURL(files[0]);
+      }
+    });
+  }
+
+  carregarImagem() {
+    this.inputFile.click();
+  }
+
+  get inputFile(): HTMLElement {
+    const inputFile: any = this.$refs.inputFile;
+    return inputFile;
+  }
+
+  get rawImg(): any {
+    const rawImg: any = this.$refs.rawImg;
+    return rawImg;
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.v-application {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
 }
-</style>
+.v-main__wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.img {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>4
---
+++ detectar-objetos-em-imagens/src/components/Layout.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="layout">
+    <div class="layout__title"><slot name="title" /></div>
+    <div class="layout__content">
+      <div class="layout__view-1">
+        <div class="layout__view-1__title layout__view__title">
+          <slot name="view1-title" />
+        </div>
+        <div class="layout__view-1__content layout__view__content">
+          <slot name="view1" />
+        </div>
+      </div>
+      <div class="layout__view-2">
+        <div class="layout__view-2__title layout__view__title">
+          <slot name="view2-title" />
+        </div>
+        <div class="layout__view-2__content layout__view__content">
+          <slot name="view2" />
+        </div>
+      </div>
+    </div>
+    <div class="layout__buttons">
+      <slot name="buttons" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped>
+.layout {
+  border: 1px solid gray;
+  width: 80%;
+  min-height: 500px;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  box-sizing: content-box;
+}
+.layout__title {
+  padding: 4px 8px;
+  font-size: 1.1em;
+  background-color: #1976d2;
+  color: white;
+  /*text-transform: uppercase;*/
+}
+.layout__content {
+  flex: 1;
+  display: flex;
+}
+.layout__content > div {
+  flex: 1;
+  background-color: #e8e8e8;
+  border-bottom: 1px solid gray;
+}
+.layout__content > div:first-child {
+  border-right: 1px solid gray;
+}
+.layout__buttons {
+  padding: 8px;
+  text-align: right;
+}
+.layout__view__title {
+  background: rgba(0, 0, 0, 0.2);
+  padding: 4px;
+  text-align: center;
+}
+.layout__view__content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+}
+</style>
